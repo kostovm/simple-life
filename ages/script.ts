9530f58d493c2cca637414720fb603f581219801
kostovm <kostov543@gmail.com>
2024-12-27 09:45:25 +0200

    Added simple app
    My first simple app for this project.

---
+++ ages/script.ts
@@ -0,0 +1,89 @@
+type Output = {
+    text: string,
+    icon: string
+};
+
+const outputs = {
+    baby: {text: 'Baby', icon: 'fa-solid fa-baby'},
+    child: {text: 'Child', icon: 'fa-solid fa-child'},
+    teenager: {text: 'Teenager', icon: 'fa-solid fa-pizza-slice'},
+    adult: {text: 'Adult', icon: 'fa-solid fa-user'},
+    elderly: {text: 'Elderly', icon: 'fa-regular fa-user'},
+    outOfBound: "Age is out of bound!",
+    emptyInput: "Enter age!"
+}
+
+const ageInput = document.getElementById('age') as HTMLInputElement | null;
+const sendButton = document.getElementById('send') as HTMLInputElement | null;
+const clearButton = document.getElementById('clear') as HTMLElement | null;
+const outputField = document.querySelector('.output-field') as HTMLElement | null;
+
+clearButton?.addEventListener('click', clearOutput);
+sendButton?.addEventListener('click', showOutput);
+
+function showOutput(): void{
+
+    if(!ageInput || !outputField) return;
+
+    outputField.replaceChildren();
+
+    const ageValue = ageInput.value.trim();
+    if(!ageValue){
+        displayOutput(outputs.emptyInput);
+        return;
+    }
+
+    const age = Number(ageValue);
+    if(isNaN(age)){
+        displayOutput('Invalid age value');
+        return;
+    }
+    
+    const {text, icon} = determineOutput(age);
+    displayOutput(text, icon);
+    ageInput.value = '';
+
+}
+
+function clearOutput(): void{
+
+    if(!outputField) return;
+
+    outputField.replaceChildren();
+    if (ageInput) ageInput.value = '';
+
+    displayOutput(outputs.emptyInput);
+}
+
+function determineOutput(age: number): {text: string; icon?: string}{
+
+    if(age < 0 || age > 120){
+        return {text: outputs.outOfBound};
+    } else if(age < 3){
+        return outputs.baby;
+    } else if(age < 14){
+        return outputs.child;
+    } else if(age < 20){
+        return outputs.teenager;
+    } else if(age < 66){
+        return outputs.adult;
+    }else {
+        return outputs.elderly;
+    }
+
+}
+
+function displayOutput(text: string, iconClass?: string){
+
+    const outputP: HTMLElement = document.createElement('p');
+
+    if(iconClass){
+        const icon = document.createElement('i');
+        icon.className = iconClass;
+        outputP.appendChild(icon);
+    }
+
+    outputP.appendChild(document.createTextNode(text));
+    outputField?.appendChild(outputP);
+
+}
