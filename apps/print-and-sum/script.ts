a9a8208b6f52f4b8afc0d7d596679b341d84d6f8
kostovm <kostov543@gmail.com>
2025-06-12 18:37:29 +0300

    Set min value for the end number
    The end number will never be less or equal to the start number.

--- apps/print-and-sum/script.ts
+++ apps/print-and-sum/script.ts
@@ -1,3 +1,23 @@
 import { displayAppInfo } from "../../shared/utils.js";
 
+const startInput = document.getElementById('start') as HTMLInputElement | null;
+const endInput = document.getElementById('end') as HTMLInputElement | null;
+
 displayAppInfo('print-and-sum');
+
+startInput?.addEventListener('change', setMinEndNum);
+
+function setMinEndNum (){
+    if (!startInput?.value){
+        endInput!.disabled = true;
+        endInput!.value = '';
+        endInput!.removeAttribute('min');
+    } else {
+        const startValue = Number(startInput.value);
+        endInput!.disabled = false;
+        endInput!.setAttribute('min', (startValue + 1).toString());
+        if (!endInput!.value || Number(endInput!.value) <= startValue){
+            endInput!.value = (startValue + 1).toString();
+        }
+    }
+}
