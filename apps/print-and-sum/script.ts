788b11e02695c88b016ebaa6395d8479126784ab
kostovm <kostov543@gmail.com>
2025-06-16 20:01:55 +0300

    Print button disabled
    Print button will be enabled only if there is  valid input.

--- apps/print-and-sum/script.ts
+++ apps/print-and-sum/script.ts
@@ -2,12 +2,16 @@ import { displayAppInfo } from "../../shared/utils.js";
 
 const startInput = document.getElementById('start') as HTMLInputElement | null;
 const endInput = document.getElementById('end') as HTMLInputElement | null;
+const printButton = document.getElementById('send') as HTMLButtonElement | null;
 
 displayAppInfo('print-and-sum');
 
 startInput?.addEventListener('change', setMinEndNum);
+startInput?.addEventListener('change', enablePrint);
+endInput?.addEventListener('change', enablePrint)
+printButton?.addEventListener('click', () => console.log('Printing!'))
 
-function setMinEndNum (){
+function setMinEndNum (): void{
     if (!startInput?.value){
         endInput!.disabled = true;
         endInput!.value = '';
@@ -19,5 +23,15 @@ function setMinEndNum (){
         if (!endInput!.value || Number(endInput!.value) <= startValue){
             endInput!.value = (startValue + 1).toString();
         }
+    }
+}
+
+function enablePrint (){
+    const startValue = startInput?.value;
+    const endValue = endInput?.value;
+    if (startValue && endValue){
+        printButton!.disabled = false;
+    }else{
+        printButton!.disabled = true;
     }
 }
