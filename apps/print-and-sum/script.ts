2d688a9cccf55a7b6e0ecd804d4528044dcfc814
kostovm <kostov543@gmail.com>
2025-06-20 17:15:00 +0300

    Added the new app to the homepage
    Users can now access the app through the menu on the main page.

--- apps/print-and-sum/script.ts
+++ apps/print-and-sum/script.ts
@@ -1,70 +1,64 @@
 import { displayAppInfo } from "../../shared/utils.js";
 
-const startInput = document.getElementById('start') as HTMLInputElement | null;
-const endInput = document.getElementById('end') as HTMLInputElement | null;
-const sumOutput = document.getElementById('sum') as HTMLSpanElement | null;
-const stringOutputField = document.querySelector('.output-field p') as HTMLElement |null;
-const printButton = document.getElementById('send') as HTMLButtonElement | null;
-const clearButton = document.getElementById('clear') as HTMLButtonElement | null;
+const startInput = document.getElementById('start') as HTMLInputElement;
+const endInput = document.getElementById('end') as HTMLInputElement;
+const sumOutput = document.getElementById('sum') as HTMLSpanElement;
+const numbersOutput = document.querySelector('.output-field p') as HTMLElement;
+const printButton = document.getElementById('send') as HTMLButtonElement;
+const clearButton = document.getElementById('clear') as HTMLButtonElement;
 
 displayAppInfo('print-and-sum');
 
-startInput?.addEventListener('change', setMinEndNum);
-startInput?.addEventListener('change', enablePrint);
-endInput?.addEventListener('change', enablePrint);
-printButton?.addEventListener('click', printNumbers);
-clearButton?.addEventListener('click', clearAll);
+startInput.addEventListener('change', updateEndInputMinAndValue);
+startInput.addEventListener('change', updatePrintButtonState);
+endInput.addEventListener('change', updatePrintButtonState);
+printButton.addEventListener('click', printNumbersAndSum);
+clearButton.addEventListener('click', clearInputsAndOutput);
 
-function clearAll(){
-    startInput!.value = "";
-    endInput!.value = "";
-    sumOutput!.textContent = "";
-    stringOutputField!.textContent = "";
+function clearInputsAndOutput(): void {
+    startInput.value = "";
+    endInput.value = "";
+    sumOutput.textContent = "";
+    numbersOutput.textContent = "";
 
-    enablePrint();
+    updateEndInputMinAndValue();
+    updatePrintButtonState();
 }
 
-function setMinEndNum (): void{
-    if (!startInput?.value){
-        endInput!.disabled = true;
-        endInput!.value = '';
-        endInput!.removeAttribute('min');
-    } else {
-        const startValue = Number(startInput.value);
-        endInput!.disabled = false;
-        endInput!.setAttribute('min', (startValue + 1).toString());
-        if (!endInput!.value || Number(endInput!.value) <= startValue){
-            endInput!.value = (startValue + 1).toString();
-        }
+function updateEndInputMinAndValue(): void {
+    const startValue = Number(startInput.value);
+
+    if (!startInput.value) {
+        endInput.disabled = true;
+        endInput.removeAttribute('min');
+        endInput.value = '';
+        return;
+    }
+
+    endInput.disabled = false;
+    endInput.min = (startValue + 1).toString();
+
+    if (!endInput.value || Number(endInput.value) <= startValue) {
+        printButton.disabled = !(startInput.value && endInput.value);
     }
 }
 
-function enablePrint (){
-    const startValue = startInput?.value;
-    const endValue = endInput?.value;
-    if (startValue && endValue){
-        printButton!.disabled = false;
-    }else{
-        printButton!.disabled = true;
-    }
+function updatePrintButtonState(): void {
+    printButton.disabled = !(startInput.value && endInput.value);
 }
 
-function printNumbers(){
-    const startValue = Number(startInput?.value);
-    const endValue = Number(endInput?.value);
+function printNumbersAndSum(): void {
+    const startValue = Number(startInput.value);
+    const endValue = Number(endInput.value);
 
     let sum = 0;
-    let stringOutput = '';
+    const numbers: number[] = [];
 
-    for (let num = startValue; num <= endValue; num++){
+    for (let num = startValue; num <= endValue; num++) {
         sum += num;
-        if(num == startValue){
-            stringOutput += num;
-        } else {
-            stringOutput += `, ${num}`;
-        }
+        numbers.push(num);
     }
 
-    sumOutput!.textContent = String(sum);
-    stringOutputField!.textContent = stringOutput;
+    sumOutput.textContent = sum.toString();
+    numbersOutput.textContent = numbers.join(', ');
 }
