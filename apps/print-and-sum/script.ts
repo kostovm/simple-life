5e6f7b959ebf3857ba36aa670ba272d974894f50
kostovm <kostov543@gmail.com>
2025-06-20 14:04:45 +0300

    Update script.ts

--- apps/print-and-sum/script.ts
+++ apps/print-and-sum/script.ts
@@ -2,14 +2,25 @@ import { displayAppInfo } from "../../shared/utils.js";
 
 const startInput = document.getElementById('start') as HTMLInputElement | null;
 const endInput = document.getElementById('end') as HTMLInputElement | null;
+const sumOutput = document.getElementById('sum') as HTMLSpanElement | null;
+const stringOutputField = document.querySelector('.output-field p') as HTMLElement |null;
 const printButton = document.getElementById('send') as HTMLButtonElement | null;
+const clearButton = document.getElementById('clear') as HTMLButtonElement | null;
 
 displayAppInfo('print-and-sum');
 
 startInput?.addEventListener('change', setMinEndNum);
 startInput?.addEventListener('change', enablePrint);
-endInput?.addEventListener('change', enablePrint)
-printButton?.addEventListener('click', () => console.log('Printing!'))
+endInput?.addEventListener('change', enablePrint);
+printButton?.addEventListener('click', printNumbers);
+clearButton?.addEventListener('click', clearAll);
+
+function clearAll(){
+    startInput!.value = "";
+    endInput!.value = "";
+    sumOutput!.textContent = "";
+    stringOutputField!.textContent = "";
+}
 
 function setMinEndNum (): void{
     if (!startInput?.value){
@@ -34,4 +45,23 @@ function enablePrint (){
     }else{
         printButton!.disabled = true;
     }
+}
+
+function printNumbers(){
+    const startValue = Number(startInput?.value);
+    const endValue = Number(endInput?.value);
+
+    let sum = 0;
+    let stringOutput = '';
+
+    for (let num = startValue; num <= endValue; num++){
+        sum += num;
+        if(num == startValue){
+            stringOutput += num;
+        } else {
+            stringOutput += `, ${num}`;
+        }
+    }
+
+    console.log('Sum: ' + sum + ', Output: ' + stringOutput);
 }
